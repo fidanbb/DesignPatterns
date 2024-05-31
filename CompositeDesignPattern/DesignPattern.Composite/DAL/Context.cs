b0ea8fa951342ac96d74e0391887317f459b3d2b
fidanbb <fidanbb@code.edu.az>
2024-05-31 18:03:22 +0400

    added compositeDesignPattern

---
+++ CompositeDesignPattern/DesignPattern.Composite/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Composite.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-LS1LSLB;initial Catalog=CompositeDB; " +
+                "integrated Security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
